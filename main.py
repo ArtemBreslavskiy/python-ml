6615853c02297edb7e5c89a12a878978bf3552c0
Артём Бреславский <artm.breslavskiy@gmail.com>
2026-01-14 19:27:07 +0500

    /

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import torch.nn.functional as F
+import torch
+from random import randint
+
+
+class Model:
+    def __init__(self, input_size, hidden_size, output_size):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+        self.W1 = torch.FloatTensor(input_size + 1, hidden_size).uniform_(-0.5, 0.5)
+        self.W2 = torch.FloatTensor(hidden_size + 1, output_size).uniform_(-0.5, 0.5)
+
+    def forward(self, x):
+        x = torch.cat([x, torch.tensor([1])])
