bba5eaaa45d5d21176fe10c3bc28d669bf66fbf4
Артём Бреславский <artm.breslavskiy@gmail.com>
2026-01-17 19:33:13 +0500

    /

--- main.py
+++ main.py
@@ -23,15 +23,26 @@ class MyModel(nn.Module):
 model = MyModel(3, 2, 1)
 
 optimizer = optim.RMSprop(params=model.parameters(), lr=0.01)
-loss_func = torch.nn.MSELoss
+loss_func = torch.nn.MSELoss()
 
 model.train()
+
+x_train = torch.FloatTensor([(-1, -1, -1), (-1, -1, 1), (-1, 1, -1), (-1, 1, 1), (1, -1, -1), (1, -1, 1), (1, 1, -1), (1, 1, 1)])
+y_train = torch.FloatTensor([-1, 1, -1, 1, -1, 1, -1, -1])
+total = len(y_train)
 
 for _ in range(1000):
     k = randint(0, total - 1)
     y = model(x_train[k])
+    y = y.squeeze()
     loss = loss_func(y, y_train[k])
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
+model.eval()
+
+for x, y in zip(x_train, y_train):
+    d = model(x)
+    print(f"Ответ:{y} | Предсказание:{d.data}")
