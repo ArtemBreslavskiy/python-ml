191648f1b4b8a1966bc088bc71ca5b9467ac537c
Артём Бреславский <artm.breslavskiy@gmail.com>
2026-01-19 16:44:27 +0500

    /

--- main.py
+++ main.py
@@ -9,38 +9,12 @@ import torch.utils.data as data
 import torchvision.transforms.v2 as tfs
 import torch.nn as nn
 import torch.optim as optim
+from torchvision.datasets import ImageFolder
 
 
-class DigitDataset(data.Dataset):
-    def __init__(self, path, train=True, transform=None):
-        self.path = os.path.join(path, "train" if train else "test")
-        self.transform = transform
-
-        with open(os.path.join(path, "format.json"), "r") as fp:
-            self.format = json.load(fp)
-
-        self.length = 0
-        self.files = []
-        self.targets = torch.eye(10)
-
-        for _dir, _target in self.format.items():
-            path = os.path.join(self.path, _dir)
-            list_files = os.listdir(path)
-            self.length += len(list_files)
-            self.files.extend(map(lambda _x: (os.path.join(path, _x), _target), list_files))
-
-    def __getitem__(self, item):
-        path_file, target = self.files[item]
-        t = self.targets[target]
-        img = Image.open(path_file)
-
-        if self.transform:
-            img = self.transform(img).ravel().float() / 255.0
-
-        return img, t
-
-    def __len__(self):
-        return self.length
+class RavelTransform(nn.Module):
+    def forward(self, item):
+        return item.ravel()
 
 
 class DigitNN(nn.Module):
@@ -58,8 +32,11 @@ class DigitNN(nn.Module):
 
 model = DigitNN(28 * 28, 32, 10)
 
-to_tensor = tfs.ToImage()
-d_train = DigitDataset("dataset", transform=to_tensor)
+transforms = tfs.Compose([tfs.ToImage(), tfs.Grayscale(),
+                         tfs.ToDtype(torch.float32, scale=True),
+                          RavelTransform()
+                          ])
+d_train = ImageFolder("dataset/train", transform=transforms)
 train_data = data.DataLoader(d_train, batch_size=32, shuffle=True)
 
 optimizer = optim.Adam(params=model.parameters(), lr=0.01)
@@ -86,7 +63,7 @@ for _e in range(epochs):
         loss_mean = 1 / lm_count * loss.item() + (1 - 1 / lm_count) * loss_mean
         train_tdqm.set_description(f"Epoch [{_e + 1}/{epochs}], loss_mean={loss_mean:.3f}")
 
-d_test = DigitDataset("dataset", train=False, transform=to_tensor)
+d_test = ImageFolder("dataset/test", transform=transforms)
 test_data = data.DataLoader(d_test, batch_size=500, shuffle=False)
 Q = 0
 
@@ -96,8 +73,7 @@ for x_test, y_test in test_data:
     with torch.no_grad():
         p = model(x_test)
         p = torch.argmax(p, dim=1)
-        y = torch.argmax(y_test, dim=1)
-        Q += torch.sum(p == y).item()
+        Q += torch.sum(p == y_test).item()
 
 Q /= len(d_test)
 print(Q)
