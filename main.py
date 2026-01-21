eff33cb37cb926ecf6bcd8453547028b046a6a52
Артём Бреславский <artm.breslavskiy@gmail.com>
2026-01-21 17:45:29 +0500

    /

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import json
 from PIL import Image
 
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 import torch
 import torch.utils.data as data
 import torchvision.transforms.v2 as tfs
+import torchvision
 import torch.nn as nn
 import torch.optim as optim
 from torchvision.datasets import ImageFolder
 
 
 class RavelTransform(nn.Module):
-    def forward(self, item):
-        return item.ravel()
+    def forward(self, item): return item.ravel()
 
 
 class DigitNN(nn.Module):
@@ -22,34 +23,41 @@ class DigitNN(nn.Module):
         super().__init__()
         self.layer1 = nn.Linear(input_dim, hidden_dim)
         self.layer2 = nn.Linear(hidden_dim, output_dim)
+        self.dropout_1 = nn.Dropout(0.3)
 
     def forward(self, x):
         x = self.layer1(x)
         x = nn.functional.relu(x)
+        x = self.dropout_1(x)
         x = self.layer2(x)
         return x
 
 
-model = DigitNN(28 * 28, 32, 10)
+model = DigitNN(28 * 28, 128, 10)
 
 transforms = tfs.Compose([tfs.ToImage(), tfs.Grayscale(),
                          tfs.ToDtype(torch.float32, scale=True),
                           RavelTransform()
                           ])
-d_train = ImageFolder("dataset/train", transform=transforms)
+
+dataset_mnist = torchvision.datasets.MNIST(r'C:/datasets/mnist', download=True, train=True, transform=transforms)
+d_train, d_val = data.random_split(dataset_mnist, [0.7, 0.3])
 train_data = data.DataLoader(d_train, batch_size=32, shuffle=True)
+train_data_val = data.DataLoader(d_val, batch_size=32, shuffle=False)
 
 optimizer = optim.Adam(params=model.parameters(), lr=0.01)
 loss_function = nn.CrossEntropyLoss()
+epochs = 20
 
-epochs = 2
-model.train()
+loss_lst_val = []
+loss_lst = []
 
 for _e in range(epochs):
+    model.train()
     loss_mean = 0
     lm_count = 0
 
-    train_tdqm = tqdm(train_data, leave=True)
+    train_tdqm = tqdm(train_data, leave=False)
 
     for x_train, y_train in train_tdqm:
         predict = model(x_train)
@@ -63,10 +71,28 @@ for _e in range(epochs):
         loss_mean = 1 / lm_count * loss.item() + (1 - 1 / lm_count) * loss_mean
         train_tdqm.set_description(f"Epoch [{_e + 1}/{epochs}], loss_mean={loss_mean:.3f}")
 
+    model.eval()
+    Q_val = 0
+    count_val = 0
+
+    for x_val, y_val in train_data_val:
+        with torch.no_grad():
+            p = model(x_val)
+            loss = loss_function(p, y_val)
+            Q_val += loss.item()
+            count_val += 1
+
+    Q_val /= count_val
+
+    loss_lst.append(loss_mean)
+    loss_lst_val.append(Q_val)
+
+    print(f" | loss_mean={loss_mean:.3f}, Q_val={Q_val:.3f}")
+
 d_test = ImageFolder("dataset/test", transform=transforms)
 test_data = data.DataLoader(d_test, batch_size=500, shuffle=False)
-Q = 0
 
+Q = 0
 model.eval()
 
 for x_test, y_test in test_data:
@@ -76,6 +102,9 @@ for x_test, y_test in test_data:
         Q += torch.sum(p == y_test).item()
 
 Q /= len(d_test)
-print(Q)
+print(f"{Q * 100}%")
 
-torch.save(model.state_dict(), "model_dnn.tar")
+plt.plot(loss_lst)
+plt.plot(loss_lst_val)
+plt.grid()
+plt.show()
