8b80478902d5597c1b2d86fa61d16028db792896
Артём Бреславский <artm.breslavskiy@gmail.com>
2026-01-22 20:23:25 +0500

    /

--- main.py
+++ main.py
@@ -2,64 +2,70 @@ import os
 import json
 from PIL import Image
 
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-
 import torch
+import torch.nn as nn
 import torch.utils.data as data
 import torchvision.transforms.v2 as tfs
-import torchvision
-import torch.nn as nn
 import torch.optim as optim
-from torchvision.datasets import ImageFolder
+from tqdm import tqdm
 
 
-class RavelTransform(nn.Module):
-    def forward(self, item): return item.ravel()
+class SunDataset(data.Dataset):
+    def __init__(self, path, train=True, transform=None):
+        self.path = os.path.join(path, "train" if train else "test")
+        self.transform = transform
+
+        with open(os.path.join(self.path, "format.json"), "r") as fp:
+            self.format = json.load(fp)
+
+            self.length = len(self.format)
+            self.files = tuple(self.format.keys())
+            self.targets = tuple(self.format.values())
+
+    def __getitem__(self, item):
+        path_file = os.path.join(self.path, self.files[item])
+        img = Image.open(path_file).convert('RGB')
+
+        if self.transform:
+            img = self.transform(img)
+
+            return img, torch.tensor(self.targets[item], dtype=torch.float32)
+
+    def __len__(self):
+        return self.length
 
 
-class DigitNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super().__init__()
-        self.layer1 = nn.Linear(input_dim, hidden_dim)
-        self.layer2 = nn.Linear(hidden_dim, output_dim)
-        self.dropout_1 = nn.Dropout(0.3)
-
-    def forward(self, x):
-        x = self.layer1(x)
-        x = nn.functional.relu(x)
-        x = self.dropout_1(x)
-        x = self.layer2(x)
-        return x
-
-
-model = DigitNN(28 * 28, 128, 10)
-
-transforms = tfs.Compose([tfs.ToImage(), tfs.Grayscale(),
-                         tfs.ToDtype(torch.float32, scale=True),
-                          RavelTransform()
-                          ])
-
-dataset_mnist = torchvision.datasets.MNIST(r'C:/datasets/mnist', download=True, train=True, transform=transforms)
-d_train, d_val = data.random_split(dataset_mnist, [0.7, 0.3])
+transforms = tfs.Compose([tfs.ToImage(), tfs.ToDtype(torch.float32, scale=True)])
+d_train = SunDataset("dataset_reg", transform=transforms)
 train_data = data.DataLoader(d_train, batch_size=32, shuffle=True)
-train_data_val = data.DataLoader(d_val, batch_size=32, shuffle=False)
 
-optimizer = optim.Adam(params=model.parameters(), lr=0.01)
-loss_function = nn.CrossEntropyLoss()
-epochs = 20
+model = nn.Sequential(
+    nn.Conv2d(3, 32, 3, padding='same'),
+    nn.ReLU(),
+    nn.MaxPool2d(2),
+    nn.Conv2d(32, 8, 3, padding='same'),
+    nn.ReLU(),
+    nn.MaxPool2d(2),
+    nn.Conv2d(8, 4, 3, padding='same'),
+    nn.ReLU(),
+    nn.MaxPool2d(2),
+    nn.Flatten(),
+    nn.Linear(4096, 128),
+    nn.Linear(128, 2)
+)
 
-loss_lst_val = []
-loss_lst = []
+optimizer = optim.Adam(params=model.parameters(), lr=0.001, weight_decay=0.001)
+loss_function = nn.MSELoss()
+
+epochs = 5
+model.train()
 
 for _e in range(epochs):
-    model.train()
     loss_mean = 0
     lm_count = 0
 
-    train_tdqm = tqdm(train_data, leave=False)
-
-    for x_train, y_train in train_tdqm:
+    train_tqdm = tqdm(train_data, leave=True)
+    for x_train, y_train in train_tqdm:
         predict = model(x_train)
         loss = loss_function(predict, y_train)
 
@@ -68,43 +74,25 @@ for _e in range(epochs):
         optimizer.step()
 
         lm_count += 1
-        loss_mean = 1 / lm_count * loss.item() + (1 - 1 / lm_count) * loss_mean
-        train_tdqm.set_description(f"Epoch [{_e + 1}/{epochs}], loss_mean={loss_mean:.3f}")
+        loss_mean = 1/lm_count * loss.item() + (1 - 1/lm_count) * loss_mean
+        train_tqdm.set_description(f"Epoch [{_e+1}/{epochs}], loss_mean={loss_mean:.3f}")
 
-    model.eval()
-    Q_val = 0
-    count_val = 0
+st = model.state_dict()
+torch.save(st, 'model_sun.tar')
 
-    for x_val, y_val in train_data_val:
-        with torch.no_grad():
-            p = model(x_val)
-            loss = loss_function(p, y_val)
-            Q_val += loss.item()
-            count_val += 1
-
-    Q_val /= count_val
-
-    loss_lst.append(loss_mean)
-    loss_lst_val.append(Q_val)
-
-    print(f" | loss_mean={loss_mean:.3f}, Q_val={Q_val:.3f}")
-
-d_test = ImageFolder("dataset/test", transform=transforms)
-test_data = data.DataLoader(d_test, batch_size=500, shuffle=False)
+d_test = SunDataset("dataset_reg", train=False, transform=transforms)
+test_data = data.DataLoader(d_test, batch_size=50, shuffle=False)
 
 Q = 0
+count = 0
 model.eval()
 
-for x_test, y_test in test_data:
+test_tqdm = tqdm(train_data, leave=True)
+for x_test, y_test in test_tqdm:
     with torch.no_grad():
         p = model(x_test)
-        p = torch.argmax(p, dim=1)
-        Q += torch.sum(p == y_test).item()
+        Q += loss_function(p, y_test).item()
+        count += 1
 
-Q /= len(d_test)
-print(f"{Q * 100}%")
-
-plt.plot(loss_lst)
-plt.plot(loss_lst_val)
-plt.grid()
-plt.show()
+Q /= count
+print(Q)
