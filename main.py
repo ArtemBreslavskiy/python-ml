bd8d321513a661f0712e451733538bc9a21bff69
Артём Бреславский <artm.breslavskiy@gmail.com>
2026-02-05 19:05:31 +0500

    /

--- main.py
+++ main.py
@@ -1,114 +1,163 @@
-from PIL import Image
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from tqdm import tqdm
 import torch
-from torchvision import models
-import torchvision.transforms.v2 as tfs_v2
+import torch.utils.data as data
+import torchvision
+import torchvision.transforms.v2 as tfs_2
 import torch.nn as nn
 import torch.optim as optim
 
 
-class ModelStyle(nn.Module):
-    def __init__(self):
-        super().__init__()
-        _model = models.vgg19(weights=models.VGG19_Weights.DEFAULT)
-        self.mf = _model.features
-        self.mf.requires_grad_(False)
-        self.requires_grad_(False)
-        self.mf.eval()
-        self.idx_out = (0, 5, 10, 19, 28, 34)
-        self.num_style_layers = len(self.idx_out) - 1
+class DigitDataset(data.Dataset):
+    def __init__(self, path, train=True, target=5, transform=False):
+        _dataset = torchvision.datasets.MNIST(path, download=True, train=train)
+        self.dataset = _dataset.data[_dataset.targets == target]
+        self.length = self.dataset.size(0)
+        self.target = torch.tensor([target], dtype=torch.float32)
 
-    def forward(self, x):
-        outputs = []
-        for indx, layer in enumerate(self.mf):
-            x = layer(x)
-            if indx in self.idx_out:
-                outputs.append(x.squeeze(0))
+        if transform:
+            self.dataset = transform(self.dataset).view(-1, 1, 28, 28)
 
-        return outputs
+    def __getitem__(self, item):
+        return self.dataset[item], self.target
+
+    def __len__(self):
+        return self.length
 
 
-def get_content_loss(base_content, target):
-    return torch.mean(torch.squeeze(base_content - target))
+model_gen = nn.Sequential(
+    nn.Linear(2, 512*7*7, bias=False),
+    nn.ELU(inplace=True),
+    nn.BatchNorm1d(512*7*7),
+    nn.Unflatten(1, (512, 7, 7)),
+    nn.Conv2d(512, 256, 5, 1, padding='same', bias=False),
+    nn.ELU(inplace=True),
+    nn.BatchNorm2d(256),
+    nn.Conv2d(256, 128, 5, 1, padding='same', bias=False),
+    nn.ELU(inplace=True),
+    nn.BatchNorm2d(128),
+    nn.ConvTranspose2d(128, 64, 4, 2, padding=1, bias=False),
+    nn.ELU(inplace=True),
+    nn.BatchNorm2d(64),
+    nn.ConvTranspose2d(64, 32, 4, 2, padding=1, bias=False),
+    nn.ELU(inplace=True),
+    nn.BatchNorm2d(32),
+    nn.Conv2d(32, 1, 1, 1),
+    nn.Sigmoid()
+)
 
+model_dis = nn.Sequential(
+    nn.Conv2d(1, 64, 5, 2, padding=2, bias=False),
+    nn.ELU(inplace=True),
+    nn.BatchNorm2d(64),
+    nn.Conv2d(64, 128, 5, 2, padding=2, bias=False),
+    nn.ELU(inplace=True),
+    nn.BatchNorm2d(128),
+    nn.Flatten(),
+    nn.Linear(128*7*7, 1)
+)
 
-def gram_matrix(x):
-    channels = x.size(dim=0)
-    g = x.view(channels, -1)
-    gram = torch.mm(g, g.mT) / g.size(dim=1)
-    return gram
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
+model_gen.to(device)
+model_dis.to(device)
 
-def get_style_loss(base_style, gram_target):
-    style_weights = [1.0, 0.8, 0.5, 0.3, 0.1]
+epochs = 20
+hidden_dim = 2
+batch_size = 16
 
-    _loss = 0
-    i = 0
-    for base, target in zip(base_style, gram_target):
-        gram_style = gram_matrix(base)
-        _loss += style_weights[i] * torch.mean(torch.square(gram_style - target))
-        i += 1
+transforms = tfs_2.Compose([tfs_2.ToImage(), tfs_2.ToDtype(dtype=torch.float32, scale=True)])
+d_train = DigitDataset(r'C:\datasets\mnist', train=True, target=5, transform=transforms)
+train_data = data.DataLoader(d_train, batch_size=batch_size, shuffle=True, drop_last=True)
 
-    return _loss
+optimizer_gen = optim.Adam(params=model_gen.parameters(), lr=0.001)
+optimizer_dis = optim.Adam(params=model_dis.parameters(), lr=0.001)
+loss_func = nn.BCEWithLogitsLoss()
 
+targets_0 = torch.zeros(batch_size, 1).to(device)
+targets_1 = torch.ones(batch_size, 1).to(device)
 
-img = Image.open('img_3.jpg').convert('RGB')
-img_style = Image.open('img_style_2.jpg').convert('RGB')
+loss_gen_lst = []
+loss_dis_lst = []
 
-transforms = tfs_v2.Compose([tfs_v2.ToImage(),
-                             tfs_v2.ToDtype(torch.float32, scale=True)
-                             ])
-
-img = transforms(img).unsqueeze(0)
-img_style = transforms(img_style).unsqueeze(0)
-img_create = img.clone()
-img_create.requires_grad_(True)
-
-model = ModelStyle()
-
-outputs_img = model(img)
-outputs_img_style = model(img_style)
-
-gram_matrix_style = [gram_matrix(x) for x in outputs_img_style[:model.num_style_layers]]
-
-content_weight = 1
-style_weight = 1000
-best_loss = -1
-epochs = 100
-best_img = img_create.clone()
-
-optimizer = optim.Adam(params=[img_create], lr=0.01)
+model_gen.train()
+model_dis.train()
 
 for _e in range(epochs):
-    outputs_img_create = model(img_create)
+    loss_mean_gen = 0
+    loss_mean_dis = 0
+    lm_count = 0
 
-    loss_content = get_content_loss(outputs_img_create[-1], outputs_img[-1])
-    loss_style = get_style_loss(outputs_img_create, gram_matrix_style)
-    loss = content_weight * loss_content + style_weight * loss_style
+    train_tqdm = tqdm(train_data, leave=True)
+    for x_train, y_train in train_tqdm:
+        x_train = x_train.to(device)
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        h = torch.normal(mean=torch.zeros((batch_size, hidden_dim)), std=torch.ones((batch_size, hidden_dim)))
+        h = h.to(device)
 
-    img_create.data.clamp_(0, 1)
+        img_gen = model_gen(h)
+        fake_out = model_dis(img_gen)
 
-    if loss < best_loss or best_loss < 0:
-        best_loss = loss
-        best_img = img_create.clone()
+        loss_gen = loss_func(fake_out, targets_1)
 
-    print(f'Iteration: {_e}/{epochs}, loss: {loss.item():.4f}')
+        optimizer_gen.zero_grad()
+        loss_gen.backward()
+        optimizer_gen.step()
 
-x = best_img.detach().squeeze()
-low, hi = torch.amin(x), torch.amax(x)
-x = (x - low) / (hi - low) * 255.0
-x = x.permute(1, 2, 0)
-x = x.numpy()
-x = np.clip(x, 0, 255).astype('uint8')
+        # discriminator learning
+        img_gen = model_gen(h)
+        fake_out = model_dis(img_gen)
+        real_out = model_dis(x_train)
 
-image = Image.fromarray(x, 'RGB')
-image.save("result.jpg")
+        outputs = torch.cat([real_out, fake_out], dim=0).to(device)
+        targets = torch.cat([targets_1, targets_0], dim=0).to(device)
 
-plt.imshow(x)
+        loss_dis = loss_func(outputs, targets)
+
+        optimizer_dis.zero_grad()
+        loss_dis.backward()
+        optimizer_dis.step()
+
+        lm_count += 1
+        loss_mean_gen = 1/lm_count * loss_gen.item() + (1 - 1/lm_count) * loss_mean_gen
+        loss_mean_dis = 1/lm_count * loss_dis.item() + (1 - 1/lm_count) * loss_mean_dis
+
+        train_tqdm.set_description(f"Epoch [{_e+1}/{epochs}], loss_mean_gen={loss_mean_gen:.3f}, loss_mean_dis={loss_mean_dis:.3f}")
+
+    loss_gen_lst.append(loss_mean_gen)
+    loss_dis_lst.append(loss_mean_dis)
+
+st = model_gen.state_dict()
+torch.save(st, 'model_gen.tar')
+
+st = model_dis.state_dict()
+torch.save(st, 'model_dis.tar')
+
+st = {'loss_gen': loss_gen_lst, 'loss_dis': loss_dis_lst}
+torch.save(st, 'model_gan_losses.tar')
+
+model_gen.eval()
+n = 2
+total = 2*n+1
+
+plt.figure(figsize=(total, total))
+
+num = 1
+for i in range(-n, n+1):
+    for j in range(-n, n+1):
+        ax = plt.subplot(total, total, num)
+        num += 1
+        h = torch.tensor([[1 * i / n, 1 * j / n]], dtype=torch.float32)
+        predict = model_gen(h.to(device))
+        predict = predict.detach().squeeze()
+        dec_img = predict.cpu().numpy()
+
+        plt.imshow(dec_img, cmap='gray')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
 plt.show()
